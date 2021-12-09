fc1e53e944e0ad94e0223488d6a9e1d264203c42
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-09 10:58:58 +0000

    [Finish] 応用編課題1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_090628) do
+ActiveRecord::Schema.define(version: 2021_12_09_102820) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
